9f7778b6ce9420333fa63c3a6dc11819e3333663
Masakazu.iwahara <iwamasa1225328@gmail.com>
2020-06-20 08:31:00 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+#以下を追記
+Refile.secret_key = '7ca0a1a5e15c6b874db4c15eb82cb0d8c1411195af947e967523e3db217ed4e9b3f4938f64481be45fe389b458b214d088aa39c85476aa4329681ad297e8e303'
